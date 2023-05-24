5455c19345827e76f4e35047f1fb465696d09796
jose.arriagada <jose.arriagada1701@alumnos.ubiobio.cl>
2023-05-24 12:47:26 -0400

    Juego terminado

--- juego.cpp
+++ juego.cpp
@@ -34,7 +34,7 @@ void colocar_piezas(int (*tablero)[tam_tablero], int t) {
 	//, submarino 3 casillas, lancha 1 casilla.
 	int x = nAlearorio(7);
 	int y =nAlearorio(7);
-	// 0 abajo, 1 arriaba , 2 derecha y 3 izq
+	// 0 abajo arriaba , 1 derecha izquierda
 	int d = nAlearorio(1);
 	int b = 0;
 	switch (d) {
@@ -72,14 +72,92 @@ void colocar_piezas(int (*tablero)[tam_tablero], int t) {
 
 }
 
+bool perdio(int (*tablero)[tam_tablero]){
+	for(int i=0;i<tam_tablero;i++){
+		for(int j=0;j<tam_tablero;j++){
+			int aux = tablero[i][j];
+			if(aux>0) return false; // si quedan piezas
+		}
+	}
+	return true;
 
+}
 
+void disparar(int (*tablero)[tam_tablero]){
 
+	int x = nAlearorio(9);
+	int y = nAlearorio(9);
 
-int main(){
+	int aux = tablero[x][y];
+	tablero[x][y] = -1;
+	//Portaaviones 5 casillas, buque 4 casillas
+	//, submarino 3 casillas, lancha 1 casilla.
+	switch (aux) {
+	        case 0:
+	        	cout << "Haz dado al agua" << endl;
+	        	break;
+	        case 5:
+	        	cout << "Haz dado a un Portaviones" << endl;
+	        	break;
+	        case 4:
+	        	cout << "Haz dado a un Buque" << endl;
+	        	break;
+	        case 3:
+	        	cout << "Haz dado a un Submarino" << endl;
+	        	break;
+	        case 1:
+	        	cout << "Haz dado a una Lancha" << endl;
+	        default:
+	            break;
+	}
+
+}
+
+void iniciarJuego(){
+	cout << "Colocando Piezas Jugardor" << endl;
 	colocar_piezas(tablero1,5);
 	colocar_piezas(tablero1,4);
 	colocar_piezas(tablero1,3);
 	colocar_piezas(tablero1,1);
+
+	cout << "Colocando Piezas Maquina" << endl;
+	colocar_piezas(tablero2,5);
+	colocar_piezas(tablero2,4);
+	colocar_piezas(tablero2,3);
+	colocar_piezas(tablero2,1);
+
+	int b=1;
+	while(b!=0){
+
+		if(b%2==1){
+			cout << "Turno " << b << endl;
+			cout << "Disparo Juagador" << endl;
+			disparar(tablero1);
+			}
+		else{
+			cout << "Turno " << b << endl;
+			cout << "Disparo Maquina" << endl;
+			disparar(tablero2);
+			}
+		if(perdio(tablero1) || perdio(tablero2)){
+			if(perdio(tablero1))
+				cout << "Haz Perdido" << endl;
+			else cout << "Haz ganado" << endl;
+			b=0;
+		}
+		else b++;
+		
+		
+	}
+	cout << "Tablero Juagador" << endl;
 	imprimir_tablero(tablero1);
+	cout << "Tablero Maquina" << endl;
+	imprimir_tablero(tablero2);
+
+
+
+}
+
+int main(){
+	iniciarJuego();
 }
